c46d2d05e4f6783d104605ae7e48ddb9d31eaa42
Maruf Khan <collab@withmaruf.com>
2025-07-16 18:13:33 +0600

    Add migration to drop the ToDo table

--- app/models/todo.py
+++ app/models/todo.py
@@ -5,5 +5,5 @@ from sqlmodel import SQLModel, Field
 class ToDo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    description: Optional[str] = None
-    completed: bool = Field(default=False)
+    description: Optional[str]
+    completed: bool = False
